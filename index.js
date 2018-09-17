701d9c1a8af5d68229019b87afe4060d5113e37e
Andrew McGill <arm5077@gmail.com>
2018-09-17 08:00:09 -0400

    Add tranforms and classifiers

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const request = require('request-promise-native'),
   fs = require('fs'),
   papaparse = require('papaparse'),
-  variables = require('./variables.json');
+  variables = require('./variables.json'),
+  transforms = require('./transforms.json'),
+  quantiles = {
+    25: {},
+    50: {},
+    75: {},
+    90: {},
+    100: {}
+  };
+let data = [];
 
 async function init(){
   const queryString = variables.map((d) => d.id).reduce((a,b) => {
@@ -21,7 +30,6 @@ async function init(){
   })
 
   // Convert array of values into a sensical object
-  let data = [];
   result.forEach((row, i) => {
     // Skip if this is the header row
     if (i==0)
@@ -47,19 +55,104 @@ async function init(){
     });
   }).then((results) => {
     return results.data
-  })
+  });
   
   data = data.map((d) => {
     const urbanity = urban_rural.find((f) => f['2015 GEOID'] === d.STATE + d.COUNTY);
     if (urbanity) {
-      d['Percent urban'] = +urbanity['2010 Census \rPercent Rural'];
+      d['Percent urban'] = 100 - +urbanity['2010 Census \rPercent Rural'];
     } else {
       d['Percent urban'] = null;
     }
-    
     return d;
   });
   
-  fs.writeFileSync('export.json', JSON.stringify(data));
+  // Make summed columns
+  data.forEach((county) => {
+    transforms.forEach((variable) => {
+      if (variable.type !== 'addTo') {
+        return
+      }
+      let newColumn = 0;
+      variable.addTo.forEach((column) => {
+        newColumn += county[column];
+      });
+      county[variable.newName] = newColumn;
+    });
+  });
+  
+  // Calculate rates
+  data.forEach((county) => {
+    transforms.forEach((variable) => {
+      if (variable.type !== 'makeRate') {
+        return;
+      }
+      let numerator = county[variable.simpleName];
+      let rate = numerator / county[variable.denominator];
+      county[variable.rateName] = rate;
+    });
+  });
+  
+    
+  // Make quantiles
+  transforms.forEach((variable) => {
+    if (variable.type !== 'getQuartile') {
+      return;
+    }    
+    data.sort((a,b) => a[variable.simpleName] - b[variable.simpleName]);
+    const slice = data.map((d) => {
+      return d[variable.simpleName]
+    });
+      
+    [25, 50, 75, 90, 100].forEach((n) => {
+      quantiles[n][variable.simpleName] = slice[Math.floor(slice.length * n / 100)];
+    });
+  });
+  
+  // Apply county archetype labels
+  archetypes.forEach((archetype) => {
+    data.forEach((county) => {
+      county[archetype.name] = archetype.classifier(county);
+    })
+  })
+  
+  fs.writeFileSync('export.json', JSON.stringify(data, null, " "));
 };
 init();
+
+const archetypes = [
+  {
+    name: "Diverse suburb",
+    classifier: function(county) {
+      // Suburbs currently defined as being above median for percent urban but below 100%
+      const suburbs = data.filter((d) => {
+        return d['Percent urban'] > quantiles[50]['Percent urban'] && d['Percent urban'] < 100;
+      });
+      
+      // Calculates the median White NH population among suburbs.
+      const medianSuburbDiversity = suburbs.sort((a,b) => a['Percent white non-Hispanic'] - b['Percent white non-Hispanic'])[Math.round(suburbs.length / 2)]["Percent white non-Hispanic"];
+      const diverseSuburbs = suburbs.filter((d) => d['Percent white non-Hispanic'] < medianSuburbDiversity);
+      
+      // Is this county within the list of diverse suburbs?
+      return diverseSuburbs.filter((d) => d.NAME == county.NAME).length > 0;
+    }
+  },
+  {
+    name: "Majority minority",
+    classifier: function(county) {
+      return county['Percent white non-Hispanic'] < .5;
+    }
+  },
+  {
+    name: "Wealthy enclave",
+    classifier: function(county) {
+      return county['Median household income'] >= quantiles[90]['Median household income'];
+    }
+  },
+  {
+    name: "Manufacturing/mining town",
+    classifier: function(county) {
+      return county['Percent employed in manufacturing, agriculture, and mining'] >= quantiles[75]['Percent employed in manufacturing, agriculture, and mining'];
+    }
+  }
+]
